77f7ec78313585d787378dcfdd685a8305b3a643
Catalina Campos <catalina.camposcaceres9@gmail.com>
2020-03-23 00:42:06 -0300

    UI modal

--- app/src/components/Modal/ModalForm.js
+++ app/src/components/Modal/ModalForm.js
@@ -19,27 +19,30 @@ class ModalForm extends Component {
   render() {
     const { modalShow, toggle } = this.props;
     const { title } = this.state;
-    console.log(modalShow);
     return (
-      <div className={modalShow ? "modal is-active" : "modal"}>
-        <h3>Add counter</h3>
-        <div className="box-body">
-          <input
-            placeholder="Name"
-            className="box-input"
-            onChange={e => this.setState({ title: e.target.value })}
-          />
-        </div>
-        <div className="box-footer">
-          <button className="btn-cancel" onClick={() => toggle(false)}>
-            Cancel
+      <div
+        className={modalShow ? "modal-container is-active" : "modal-container"}
+      >
+        <div className="modal-content">
+          <button className="close-button" onClick={() => toggle(false)}>
+            x
           </button>
-          <button
-            className="button button--create"
-            onClick={() => this.newCounter(title)}
-          >
-            Create
-          </button>
+          <h3>Add counter</h3>
+          <div className="box-body">
+            <input
+              placeholder="Name"
+              className="box-input"
+              onChange={e => this.setState({ title: e.target.value })}
+            />
+          </div>
+          <div className="box-footer">
+            <button
+              className="button button--create"
+              onClick={() => this.newCounter(title)}
+            >
+              Create
+            </button>
+          </div>
         </div>
       </div>
     );
